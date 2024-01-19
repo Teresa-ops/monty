6c6f50e2355bb262b156dd05c3a5de3bfcbdea7e
Teresa-ops <trsmwaura@gmail.com>
2024-01-19 02:20:04 -0800

    updates

--- monty.h
+++ monty.h
@@ -1,6 +1,5 @@
 #ifndef MONTY_H
 #define MONTY_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,23 +7,21 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
-
 /**
- * struct stack_s - doubly linked list representation of a queue (or stack)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
- * @prev: points to the previous element of the queue (or stack)
- * @next: points to the next element of the queue (or stack)
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
 /**
  * struct bus_s - variables -args, file, line content
  * @arg: value
@@ -35,14 +32,12 @@ typedef struct stack_s
  */
 typedef struct bus_s
 {
-    char *arg;
-    FILE *file;
-    char *content;
-    int lifi;
-} bus_t;
-
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
 extern bus_t bus;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -53,13 +48,12 @@ extern bus_t bus;
  */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
-char *clean_line(char *content);
+char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
@@ -81,6 +75,5 @@ void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
-
 #endif
 
